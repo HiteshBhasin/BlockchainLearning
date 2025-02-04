3d20b1f9895ec421f66b9e8c8aa70294d838e99e
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-02-04 11:57:21 +0000

    this is a new commit

--- blockchain.ts
+++ blockchain.ts
@@ -2,3 +2,8 @@
 // So it begins with one block called the genesis block. 
 // Among other things, a block contains a hash, the previous block hash, 
 // and at least one transaction.
+// Every block in the blockchain keeps a record of its hash and the previous 
+// block’s hash to keep the network safe from hackers
+// This implies that for a hacker to gain access and break the network, 
+// they need to generate the hashes and match them to 
+// the right block without breaking other blocks
