f486a6b1f320b09f1c709c9cb9343d5214a8ad59
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-02-04 11:55:41 +0000

    new commit

--- blockchain.ts
+++ blockchain.ts
@@ -1,1 +1,4 @@
-// A blockchain is a connection of many blocks. So it begins with one block called the genesis block. Among other things, a block contains a hash, the previous block hash, and at least one transaction.
+// A blockchain is a connection of many blocks. 
+// So it begins with one block called the genesis block. 
+// Among other things, a block contains a hash, the previous block hash, 
+// and at least one transaction.
