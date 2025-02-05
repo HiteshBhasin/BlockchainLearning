5ad57431cbebb470af01db8f849c36486dd5d326
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-02-05 15:58:03 +0000

    new commit

--- decorativePatterns/baverage.java
+++ decorativePatterns/baverage.java
@@ -1,10 +1,19 @@
-
 public abstract class baverage {
-    String discription = " ";
-   
 
+   
+    public enum size{TALL, GRANDE , VENTI};
+   size s = size.TALL;
+   String discription = " ";
     public String discription(){
         return discription;
+    }
+    
+    public size getS() {
+        return s;
+    }
+
+    public void setS(size s) {
+        this.s = s;
     }
     public abstract double  cost();
 }
